ebdc558bbdacaae83376107da81faac7923a40fc
niniupiwofu <593244237@qq.com>
2017-06-21 22:15:31 +0800

    二D项目

--- University_inquiry/University_inquiry.py
+++ University_inquiry/University_inquiry.py
@@ -24,14 +24,22 @@ def entry_page() -> 'html':
 def pick_a_color() -> 'html':
 	user_school_name= request.form['user_school']
 	user_school_info=[]
+	
 	for i in range(len(s_data)):
 		gg=gd_s.data[i]
 		if user_school_name in s_data[i]['院校名称']:
 			user_school_info.append(gg)
+	number_1=user_school_info[0]['数量（个）']
+	number_2=user_school_info[1]['数量（个）']
+	number_3=user_school_info[2]['数量（个）']
+	number_4=user_school_info[3]['数量（个）']
 	return render_template('results.html',
-							the_title = '以下是您选取的院校信息：',
-							the_school_info = user_school_info,
+							the_title = '以下是您选取的院校信息',
 							the_school_name = user_school_name,
+							the_school_1=number_1,
+							the_school_2=number_2,
+							the_school_3=number_3,
+							the_school_4=number_4,
 							)
 
 if __name__ == '__main__':
